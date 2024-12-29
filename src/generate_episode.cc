c6172e38d0bdac2c262b7481fe1982d1d6ab721b
dolfun <abhinav150104@gmail.com>
2024-12-29 20:32:58 +0530

    Implement games generation from a pool

--- src/generate_episode.cc
+++ src/generate_episode.cc
@@ -2,6 +2,20 @@
 #include "mcts/generate_episode.h"
 #include <random>
 
+constexpr int pool_size = 16;
+static auto seeds = [] {
+  std::array<unsigned int, pool_size> seeds{};
+
+  unsigned int seed = 2389473453;
+  std::mt19937 engine { seed };
+  std::uniform_int_distribution<unsigned int> dist;
+  for (auto& seed : seeds) {
+    seed = dist(engine);
+  }
+
+  return seeds;
+} ();
+
 auto generate_episode(
   int simulations_per_move, int thread_count, 
   float c_puct, int virtual_loss,
@@ -9,16 +23,18 @@ auto generate_episode(
     -> std::vector<mcts::Evaluation<Container>> {
 
   static std::random_device rd{};
-  static std::uniform_int_distribution<int> dist { 4, 8 };
+  static std::mt19937 seed_engine { rd() };
+  static std::uniform_int_distribution<int> seed_dist { 0, pool_size - 1 };
 
-  unsigned int seed = rd();
+  unsigned int seed = 3502187940;
   std::mt19937 engine { seed };
+  std::uniform_int_distribution<int> dist { 2, 5 };
   std::vector<Package> packages(Container::package_count);
   for (auto& package : packages) {
     package.shape = { dist(engine), dist(engine), dist(engine) };
   }
 
-  int container_height = 24;
+  int container_height = 10;
   Container container { container_height, packages };
   EvaluationQueue evaluation_queue { batch_size, evaluate };
   auto result = mcts::generate_episode(
