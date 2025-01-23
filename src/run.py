c224ad30bf170c7778366c3474d562e0bafcf3b3
dolfun <abhinav150104@gmail.com>
2025-01-23 21:12:26 +0530

    Save a checkpoint every iteration

--- src/run.py
+++ src/run.py
@@ -33,11 +33,12 @@ class Config:
   virtual_loss: int
   alpha: float
 
-def perform_iteration(config: Config, model_path: str, generate_only: bool):
+def perform_iteration(idx: int, config: Config, model_path: str, generate_only: bool):
   # Simulate Games
   print('SIMULATING GAMES:')
   episodes = generate_episodes(config, model_path, device)
-  with open('episodes_train.bin', 'wb') as f:
+
+  with open(f'checkpoints/episodes_train{idx}.bin', 'wb') as f:
     pickle.dump(episodes, f)
 
   print()
@@ -48,6 +49,7 @@ def perform_iteration(config: Config, model_path: str, generate_only: bool):
   print('TRAINING:')
   model = PolicyValueNetwork().to(device)
   model.load_state_dict(torch.load(model_path, weights_only=False))
+  torch.save(model.state_dict(), f'checkpoints/model{idx}.pth')
   train_policy_value_network(model, episodes, device)
   torch.save(model.state_dict(), model_path)
   print()
@@ -60,7 +62,7 @@ def perform_iteration(config: Config, model_path: str, generate_only: bool):
   eval_config.alpha = 0
 
   episodes = generate_episodes(eval_config, model_path, device, leave_progress_bar=False)
-  with open('episodes_test.bin', 'wb') as f:
+  with open(f'checkpoints/episodes_test{idx}.bin', 'wb') as f:
     pickle.dump(episodes, f)
 
   print()
@@ -75,17 +77,17 @@ def main():
   config = Config(
     seed=2389473453,
     seed_pool_size=2048,
-    episodes_per_iteration=1024,
+    episodes_per_iteration=2048,
     processes=4,
     step_size=32,
     workers_per_process=16,
     move_threshold=4,
-    simulations_per_move=400,
+    simulations_per_move=512,
     mcts_thread_count=8,
     batch_size=32,
     c_puct=1.25,
     virtual_loss=3,
-    alpha=0.1
+    alpha=0.15
   )
 
   # Create model if does not exist
@@ -94,9 +96,10 @@ def main():
     model = PolicyValueNetwork().to(device)
     torch.save(model.state_dict(), args.model_path)
 
+  os.makedirs('checkpoints', exist_ok=True)
   for i in range(args.iteration_count):
     print(f'[{i + 1}/{args.iteration_count}]')
-    perform_iteration(config, args.model_path, args.generate_only)
+    perform_iteration(i + 1, config, args.model_path, args.generate_only)
 
 if __name__ == '__main__':
   main()
